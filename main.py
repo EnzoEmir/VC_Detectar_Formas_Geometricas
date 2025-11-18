d04ecc28c9d92189622e75c286c9c832103ad95a
ENZO EMIR VIANA FERRAZ <164296530+EnzoEmir@users.noreply.github.com>
2025-11-18 09:09:31 -0300

    feat: Transformar para HSV

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ imagem = cv2.imread('assets/13_frame_000001_t1.50s.jpg')
 # Mostra a imagem na tela
 cv2.imshow('Minha Imagem', imagem)
 
+# RBG para HSV 
+imagem_hsv = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
+cv2.imshow('Convertida (HSV)', imagem_hsv)
+
 # Deixa a janela aberta
 cv2.waitKey(0)
