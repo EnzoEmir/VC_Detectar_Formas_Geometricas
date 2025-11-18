caa9ca1f2ae8f2dff91c26ce79070001a2ae1e74
ENZO EMIR VIANA FERRAZ <164296530+EnzoEmir@users.noreply.github.com>
2025-11-18 09:29:49 -0300

    feat: Primeira máscara

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # Carrega a imagem
 imagem = cv2.imread('assets/13_frame_000001_t1.50s.jpg')
@@ -9,6 +10,14 @@ cv2.imshow('Minha Imagem', imagem)
 # RBG para HSV 
 imagem_hsv = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
 cv2.imshow('Convertida (HSV)', imagem_hsv)
+
+
+# Valores de azul
+azul_minimo = np.array([100, 100, 50])
+azul_maximo = np.array([140, 255, 255])
+
+mascara = cv2.inRange(imagem_hsv, azul_minimo, azul_maximo)
+cv2.imshow('Mascara', mascara)
 
 # Deixa a janela aberta
 cv2.waitKey(0)
