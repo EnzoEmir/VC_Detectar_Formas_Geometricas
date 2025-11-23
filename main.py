8e9947e0030b9feb74e6c54db105bc1541b72cb3
ENZO EMIR VIANA FERRAZ <164296530+EnzoEmir@users.noreply.github.com>
2025-11-23 16:01:49 -0300

    feat: Trackbars

--- main.py
+++ main.py
@@ -4,20 +4,44 @@ import numpy as np
 # Carrega a imagem
 imagem = cv2.imread('assets/13_frame_000001_t1.50s.jpg')
 
-# Mostra a imagem na tela
-cv2.imshow('Minha Imagem', imagem)
 
-# RBG para HSV 
-imagem_hsv = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
-cv2.imshow('Convertida (HSV)', imagem_hsv)
+cv2.namedWindow('Meus Ajustes')
 
+def nada(x):
+    pass
 
-# Valores de azul
-azul_minimo = np.array([100, 100, 50])
-azul_maximo = np.array([140, 255, 255])
+# Barras para o H (Matiz - Cor)
+cv2.createTrackbar('H Minimo', 'Meus Ajustes', 0, 179, nada) 
+cv2.createTrackbar('H Maximo', 'Meus Ajustes', 179, 179, nada)
 
-mascara = cv2.inRange(imagem_hsv, azul_minimo, azul_maximo)
-cv2.imshow('Mascara', mascara)
+# Barras para o S (Saturação - Pureza da cor)
+cv2.createTrackbar('S Minimo', 'Meus Ajustes', 0, 255, nada) 
+cv2.createTrackbar('S Maximo', 'Meus Ajustes', 255, 255, nada)
 
-# Deixa a janela aberta
-cv2.waitKey(0)
+# Barras para o V (Valor - Brilho/Luz)
+cv2.createTrackbar('V Minimo', 'Meus Ajustes', 0, 255, nada)
+cv2.createTrackbar('V Maximo', 'Meus Ajustes', 255, 255, nada)
+
+# Loop que atualiza os valores dos trackbars
+while True:
+    h_min = cv2.getTrackbarPos('H Minimo', 'Meus Ajustes')
+    h_max = cv2.getTrackbarPos('H Maximo', 'Meus Ajustes')
+    s_min = cv2.getTrackbarPos('S Minimo', 'Meus Ajustes')
+    s_max = cv2.getTrackbarPos('S Maximo', 'Meus Ajustes')
+    v_min = cv2.getTrackbarPos('V Minimo', 'Meus Ajustes')
+    v_max = cv2.getTrackbarPos('V Maximo', 'Meus Ajustes')
+
+    minimo = np.array([h_min, s_min, v_min])
+    maximo = np.array([h_max, s_max, v_max])
+
+    hsv = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
+    
+    mascara = cv2.inRange(hsv, minimo, maximo)
+
+    cv2.imshow('Minha Imagem', imagem)
+    cv2.imshow('Mascara', mascara)
+
+    # Q para sair do loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
