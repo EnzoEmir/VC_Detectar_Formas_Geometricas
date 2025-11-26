ad3a68166a78191a245048e5a98443cd1fc978e5
ENZO EMIR VIANA FERRAZ <164296530+EnzoEmir@users.noreply.github.com>
2025-11-25 23:48:23 -0300

    feat: Quadrado Roxo e Algoritmo de aproximação de cor

---
+++ descobrir_cor.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+imagem = cv2.imread('assets/13_frame_000001_t1.50s.jpg')
+
+bbox = cv2.selectROI("Selecione a Cor", imagem, showCrosshair=True)
+cv2.destroyWindow("Selecione a Cor")
+
+# Verificar se algo foi selecionado
+if bbox[2] > 0 and bbox[3] > 0:
+    x, y, w, h = bbox
+    
+
+    recorte = imagem[y:y+h, x:x+w]
+    
+    recorte_hsv = cv2.cvtColor(recorte, cv2.COLOR_BGR2HSV)
+    
+    min_hsv = np.min(recorte_hsv, axis=(0,1))
+    max_hsv = np.max(recorte_hsv, axis=(0,1))
+    
+
+    print(f"Minimo: np.array([{min_hsv[0]}, {min_hsv[1]}, {min_hsv[2]}])")
+    print(f"Maximo: np.array([{max_hsv[0]}, {max_hsv[1]}, {max_hsv[2]}])")
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- deteccao.py
+++ deteccao.py
@@ -5,28 +5,37 @@ imagem = cv2.imread('assets/13_frame_000001_t1.50s.jpg')
 
 hsv = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
 
-minimo = np.array([112, 229, 212])
-maximo = np.array([150, 255, 255])
+# Estrutura: ("Nome", [min H,S,V], [max H,S,V])
+lista_cores = [
+    ("Circulo Azul", [112, 229, 212], [150, 255, 255]),
+    ("Quadrado Roxo", [124, 162, 42], [161, 233, 118])
+]
 
-mascara = cv2.inRange(hsv, minimo, maximo)
+for nome_cor, valor_min, valor_max in lista_cores:
 
-# Limpeza Morfologia
-# Cria um "pincel" 5x5 pixels
-kernel = np.ones((5,5), np.uint8)
+    minimo = np.array(valor_min)
+    maximo = np.array(valor_max)
 
-# Erode: suaviza as bordas 
-mascara = cv2.erode(mascara, kernel, iterations=1)
-# Dilate: "Incha" a forma para tapar buracos dentro dela
-mascara = cv2.dilate(mascara, kernel, iterations=1)
+    mascara = cv2.inRange(hsv, minimo, maximo)
 
-# RETR_EXTERNAL: Pega apenas o contorno de fora 
-# CHAIN_APPROX_SIMPLE: guarda apenas os pontos essenciais da linha
-contornos, _ = cv2.findContours(mascara, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Limpeza Morfologia
+    # Cria um "pincel" 5x5 pixels
+    kernel = np.ones((5,5), np.uint8)
 
-cv2.drawContours(imagem, contornos, -1, (0, 255, 0), 3)
+    # Erode: suaviza as bordas 
+    mascara = cv2.erode(mascara, kernel, iterations=1)
+    # Dilate: "Incha" a forma para tapar buracos dentro dela
+    mascara = cv2.dilate(mascara, kernel, iterations=1)
 
-# Mostra o resultado final
-cv2.imshow('Mascara Limpa', mascara)
+    # RETR_EXTERNAL: Pega apenas o contorno de fora 
+    # CHAIN_APPROX_SIMPLE: guarda apenas os pontos essenciais da linha
+    contornos, _ = cv2.findContours(mascara, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    cv2.drawContours(imagem, contornos, -1, (0, 255, 0), 3)
+
+    if len(contornos) > 0:
+            print(f"Processando {nome_cor}: Encontrei {len(contornos)} contornos.")
+
 cv2.imshow('Contorno Detectado', imagem)
 
 cv2.waitKey(0)
